f5c37be6c7ccb30120ebc1c3c6156291b9bbf0cb
leotangram <leotangram@gmail.com>
2017-12-12 19:38:35 -0500

    ya podemos hacer login y redireccionar a welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+	
+	resources :questions
+	get '/index', to: 'welcome#index', as: 'welcome'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+  	root to: "devise/sessions#new"
+	end
+
 end
