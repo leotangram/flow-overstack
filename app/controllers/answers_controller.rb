69ba0aab8cb2458e991cd90183603711b7452aa9
leotangram <leotangram@gmail.com>
2017-12-13 21:03:45 -0500

    lista de answer en question y email de usuario que creo el question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,17 @@
 class AnswersController < ApplicationController
 	def new
 	end
+
+	def create
+		@question = Question.find(params[:question_id])
+		@answer = @question.answers.new(answer_params)
+
+		@answer.save
+		redirect_to @question
+	end
+
+  private
+  	def answer_params
+    	params.require(:answer).permit(:description).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 	
-	resources :questions
+	resources :questions do 
+	  resources :answers
+	end
+	
 	get '/index', to: 'welcome#index', as: 'welcome'
 	root 'welcome#index'
-	resources :answers
+
 
   devise_for :users
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
